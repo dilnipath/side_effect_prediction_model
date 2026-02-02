e24c45b73cdaea6523209d356e740d8e66cfb3c5
dilnipath <dpathirana500@gmail.com>
2026-02-02 14:35:31 -0500

    sample data csv

--- xml_data_collection.py
+++ xml_data_collection.py
@@ -1,4 +1,5 @@
 import xml.etree.ElementTree as ET
+import csv
 
 # print("parsing")
 # tree = ET.parse('full_database.xml')
@@ -30,25 +31,45 @@ for event, elem in context:
             
             # Skip if this is a nested drug in drug-interactions (won't have these attributes)
             if name is not None and description is not None and tox is not None and properties is not None:
-                newlst = []
-                newlst.append(name.text)
-                newlst.append(description.text)
-                newlst.append(tox.text)
+                dict1 = {}
+                dict1["name"] = name.text
+                dict1["description"] = description.text
+                dict1["toxicity"] = tox.text
 
                 for prop in properties:
-                    if (prop.find("{http://www.drugbank.ca}kind")).text == "logP":
-                        newlst.append((prop.find("{http://www.drugbank.ca}value")).text)
 
-                lst.append(newlst)
+                    if prop.text is None or prop is None:
+                        continue
+                    else:
+                        kind = (prop.find("{http://www.drugbank.ca}kind"))
+                        value = (prop.find("{http://www.drugbank.ca}value"))
+
+                        if kind is not None and value is not None:
+                            if kind.text == "logP":
+                                dict1["logP"] = value.text
+                            if kind.text == "SMILES":
+                                dict1["SMILES"] = value.text
+
+                    
+                    # elif (prop.find("{http://www.drugbank.ca}kind")).text == "logP":
+                    #     newlst.append((prop.find("{http://www.drugbank.ca}value")).text)
+                    # elif (prop.find("{http://www.drugbank.ca}kind")).text == "SMILES":
+                    #     newlst.append((prop.find("{http://www.drugbank.ca}value")).text)
+
+                lst.append(dict1)
                 
                 i += 1
                 print(f"Processed drug {i}: {name.text}")
 
         elem.clear()
         
-        if i == 2:
+        if i == 100:
             break
 
 del context
 
-print(lst)
+with open("data/sample_data.csv", 'w', newline = '', encoding='utf-8') as csvfile:
+    fieldnames = ["name", "description", "toxicity", "logP", "SMILES"]
+    writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
+    writer.writeheader()
+    writer.writerows(lst)
