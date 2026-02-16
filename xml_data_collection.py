94b6a5e7e579c145936a0e9c6c9dde3b4e6bf911
dilnipath <dpathirana500@gmail.com>
2026-02-16 12:43:39 -0500

    adding -1 to unfound values

--- xml_data_collection.py
+++ xml_data_collection.py
@@ -6,7 +6,7 @@ context = ET.iterparse('full_database.xml', events=('start',))
 
 lst = []
 
-drug_names_df = pd.read_csv('side_effect_prediction_model/data/drug_keys.csv')
+drug_names_df = pd.read_csv('drug_keys_copy.csv')
 drug_names = drug_names_df['name'].tolist()
 
 
@@ -14,37 +14,47 @@ i = 0
 for event, elem in context:
     if event == 'start':
         if elem.tag == "{http://www.drugbank.ca}drug":
-            # Only process outer drug elements that have name, description, and toxicity
             name = elem.find("{http://www.drugbank.ca}name")
             description = elem.find("{http://www.drugbank.ca}description")
             tox = elem.find("{http://www.drugbank.ca}toxicity")
 
             properties = elem.find("{http://www.drugbank.ca}calculated-properties")
         
-            # Skip if this is a nested drug in drug-interactions (won't have these attributes)
-            if name is not None and description is not None and tox is not None and properties is not None:
-                dict1 = {}
-                if name.text in drug_names:
-                    dict1["name"] = name.text
+            dict1 = {}
+            
+            if name is not None and name.text in drug_names:
+                print(name.text)
+                dict1["name"] = name.text
+                
+                if description is not None:
                     dict1["description"] = description.text
+                else: 
+                    dict1["description"] = -1
+                    
+                if tox is not None:
                     dict1["toxicity"] = tox.text
+                else:
+                    dict1["toxicity"] = -1
 
+                if properties is not None:
                     for prop in properties:
-
                         if prop.text is None or prop is None:
                             continue
                         else:
                             kind = (prop.find("{http://www.drugbank.ca}kind"))
                             value = (prop.find("{http://www.drugbank.ca}value"))
-
+    
                             if kind is not None and value is not None:
                                 if kind.text == "SMILES":
                                     dict1["SMILES"] = value.text
+                else:
+                    dict1["SMILES"] = -1
 
-                    lst.append(dict1)
-                    
-                    i += 1
-                    print(f"Processed drug {i}: {name.text}")
+                print(dict1["name"])
+                lst.append(dict1)
+            
+                i += 1
+                print(f"Processed drug {i}: {name.text}")
 
         elem.clear()
         
@@ -53,7 +63,7 @@ for event, elem in context:
 
 del context
 
-with open("data/sample_data.csv", 'w', newline = '', encoding='utf-8') as csvfile:
+with open("sample_data.csv", 'w', newline = '', encoding='utf-8') as csvfile:
     fieldnames = ["name", "description", "toxicity", "SMILES"]
     writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
     writer.writeheader()
