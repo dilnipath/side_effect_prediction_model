efc13339ff34d7aa729e470e0af457797805c91f
dilnipath <dpathirana500@gmail.com>
2026-03-11 14:26:56 -0400

    made test and train files

--- embeddings_creation_files/split_ds.py
+++ embeddings_creation_files/split_ds.py
@@ -4,7 +4,7 @@ import os
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-input_path = "./data/train_test_data"
+input_path = "./data"
 base_name = os.path.splitext(os.path.basename(input_path))[0]
 output_dir = os.path.dirname(input_path)
 
