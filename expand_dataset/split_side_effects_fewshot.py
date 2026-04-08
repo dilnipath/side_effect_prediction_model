913f1ebcd22fb8b7180c085a7be4c3b59027a68d
Grace Kenney <158098304+gkenney1598@users.noreply.github.com>
2026-04-08 13:59:18 -0400

    Add db names to side effects csvs

--- expand_dataset/split_side_effects_fewshot.py
+++ expand_dataset/split_side_effects_fewshot.py
@@ -7,17 +7,18 @@ load_dotenv()
 client = anthropic.Anthropic()
 
 drug_df = pd.read_csv("./datasets/drug_side_effects_full.csv", encoding = "utf-8")
-names = drug_df[0:250]["name"]
-side_effects = drug_df[0:250]["side_effects"]
+ml_names = drug_df[250:500]["ml_name"]
+db_names = drug_df[250:500]["db_name"]
+side_effects = drug_df[250:500]["side_effects"]
 
 split_side_effects = []
 
 with open("datasets/fewshot_side_effects.csv", "a", encoding = "utf-8") as csvfile:
-    fieldnames = ["name", "side_effects"]
+    fieldnames = ["ml_name", "side_effects", "db_name"]
     writer = csv.DictWriter(csvfile, fieldnames)
     # writer.writeheader()
 
-    for i in range(2, 250):
+    for i in range(250, 251):
         dict1 = {}
         message = client.messages.create(
             model='claude-opus-4-5',
@@ -25,13 +26,15 @@ with open("datasets/fewshot_side_effects.csv", "a", encoding = "utf-8") as csvfi
             messages=[
                 {
                     'role': 'user',
-                    'content': f"I am going to give you drugs names and their corresponding side effects in a list format. I want you to return those side effects in a list format matching the one we gave you but split each side effect into the smallest possible unit that still makes sense. Do not include anything except for the list in the response. For example, ['swelling of the face, throat, tongue, lips, eyes'] becomes '['swelling of the face', 'swelling of the throat', 'swelling of the tongue', 'swelling of the lips', 'swelling of the eyes']'. ['rash; hives; hoarseness;'] becomes '['rash', 'hives', 'hoarseness']'. ['fever, cough,  or other signs of infection'] becomes '['fever', 'cough', 'signs of infection']'. If a side effect includes the drug name, replace it with 'this medication' instead. This is the drug {names[i]} and its side effects are {side_effects[i]}.",
+                    'content': f"I am going to give you drugs names and their corresponding side effects in a list format. I want you to return those side effects in a list format matching the one we gave you but split each side effect into the smallest possible unit that still makes sense. Do not include anything except for the list in the response. For example, ['swelling of the face, throat, tongue, lips, eyes'] becomes '['swelling of the face', 'swelling of the throat', 'swelling of the tongue', 'swelling of the lips', 'swelling of the eyes']'. ['rash; hives; hoarseness;'] becomes '['rash', 'hives', 'hoarseness']'. ['fever, cough,  or other signs of infection'] becomes '['fever', 'cough', 'signs of infection']'. If a side effect includes the drug name, replace it with 'this medication' instead. This is the drug {ml_names[i]} and its side effects are {side_effects[i]}.",
                 }
             ],
         )
-        print(names[i], i)
-        dict1["name"] = names[i]
+        print(ml_names[i], i)
+        dict1["ml_name"] = ml_names[i]
+        dict1["db_name"] = db_names[i]
         dict1["side_effects"] = message.content[0].text.strip()
+        
         split_side_effects.append(dict1)
 
         writer.writerow(dict1)
