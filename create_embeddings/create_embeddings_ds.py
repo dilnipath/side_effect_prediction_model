1af25c31aada16c959c399ecfd9405417bc3692a
dilnipath <dpathirana500@gmail.com>
2026-04-08 14:21:28 -0400

    trying to create 251 test.pt and train.pt

--- create_embeddings/create_embeddings_ds.py
+++ create_embeddings/create_embeddings_ds.py
@@ -13,35 +13,35 @@ embedding_dir = "./final"
 dir_name = os.path.basename(embedding_dir.rstrip("/"))
 output_file = f"{dir_name}_dataset.pt"
 
-df = pd.read_csv("./datasets/sample_dataset.csv")
+df = pd.read_csv("./datasets/dataset_251.csv")
 
 X, y = [], []
 
 # load embeddings
 def load_embedding_desc(drug_name):
-    path = os.path.join("./drug_embeddings/biobert_embeddings", f"{drug_name}.pt")
+    path = os.path.join("./drug_embeddings/SAMPLE_biobert_description_embeddings", f"{drug_name}.pt")
     return torch.load(path) if os.path.exists(path) else None
 
 def load_embedding_tox(drug_name):
-    path = os.path.join("./drug_embeddings/biobert_tox_embeddings", f"{drug_name}.pt")
+    path = os.path.join("./drug_embeddings/SAMPLE_biobert_toxicity_embeddings", f"{drug_name}.pt")
     return torch.load(path) if os.path.exists(path) else None
 
 def load_embedding_smiles(drug_name):
-    path = os.path.join("./drug_embeddings/biobert_smiles_embeddings", f"{drug_name}.pt")
+    path = os.path.join("./drug_embeddings/SAMPLE_biobert_SMILES_embeddings", f"{drug_name}.pt")
     return torch.load(path) if os.path.exists(path) else None
 
 def load_embedding_pharmaco(drug_name):
-    path = os.path.join("./drug_embeddings/biobert_pharmacodynamics_embeddings", f"{drug_name}.pt")
+    path = os.path.join("./drug_embeddings/SAMPLE_biobert_pharmacodynamics_embeddings", f"{drug_name}.pt")
     return torch.load(path) if os.path.exists(path) else None
 
 def load_embedding_halflife(drug_name):
-    path = os.path.join("./drug_embeddings/biobert_half-life_embeddings", f"{drug_name}.pt")
+    path = os.path.join("./drug_embeddings/SAMPLE_biobert_half-life_embeddings", f"{drug_name}.pt")
     return torch.load(path) if os.path.exists(path) else None
 
 # process drug pairs
 for _, row in df.iterrows():
-    name = row['name']
-    label = row['label']
+    name = row['db_name']
+    label = row['labels']
     label = ast.literal_eval(label)
     emb_description = load_embedding_desc(name)
     emb_tox= load_embedding_tox(name)
